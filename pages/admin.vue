dd8bbdb1370abb56c7ebee95b0019d5331925891
ErikOkau <erikmsorheim@yahoo.no>
2024-06-04 08:04:56 +0200

    eae

---
+++ pages/admin.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+const email = ref('')
+const password = ref('')
+const role = ref('')
+
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- server/api/auth/logout.ts
+++ server/api/auth/logout.ts
@@ -1,3 +1,8 @@
 import { prisma } from "~/server/server"
 import { decrypt, encrypt, expires } from "../../utils/auth"
 import { SignJWT, jwtVerify } from "jose"
+
+// logouthandler
+export default defineEventHandler(async (event) => {
+       
+})
