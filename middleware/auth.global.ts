97d05e0f3b0673e56148d83552b0edb28838f2ee
ErikOkau <erikmsorheim@yahoo.no>
2024-06-04 09:16:06 +0200

    auth middleware done and decrypt

---
+++ middleware/auth.global.ts
@@ -0,0 +1,34 @@
+import type { Role } from "@prisma/client"
+
+const noAuthPages = [
+    "/login",
+    "/",
+    "/artikkel",
+    "/admin"
+]
+
+const otherPages = {
+    ADMINISTRASJON: [
+        "/ansatte"
+    ],
+    MONTOR: [],
+    SALG: [],
+}
+
+
+export default defineNuxtRouteMiddleware(async (to, from) => {
+    if (process.server) return
+
+    // If the page does not require authentication, return
+    if (noAuthPages.includes(to.path)) return
+
+    // Fetch the user role
+    const res = await $fetch('/api/auth/decrypt') as { status: Number , role: Role }
+    if(res.status !== 200) return navigateTo("/login")
+
+    const allowedRoutes = otherPages[res.role] as string[]
+
+    if(!allowedRoutes.includes(from.path)) return navigateTo("/")
+    if (!allowedRoutes.includes(to.path)) return abortNavigation()
+    
+})
--- pages/admin.vue
+++ pages/admin.vue
@@ -1,26 +1,28 @@
 <script setup lang="ts">
+import { encrypt } from '~/server/utils/auth'
 
-const email = ref('')
-const password = ref('')
-const role = ref('')
 
-async function registerUser() {
-    const response = await fetch('/api/auth/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email: email.value,
-            password: password.value
-        })
-    })
+
+const newUser = ref({
+    email: '',
+    password: '',
+    role: ''
+})
+
+async function createUser() {
+
 }
 
 </script>
 
 <template>
-
+        <QForm @submit="createUser" class="q-gutter-md q-ml-xl q-mt-xl" style="max-width: 15rem;">
+            <h5>Register</h5>
+            <QInput class="inset-shadow-down" outlined v-model="newUser.email" label="Email" />
+            <QInput class="inset-shadow-down" outlined v-model="newUser.password" label="Password" type="password" />
+            <QInput class="inset-shadow-down" outlined v-model="newUser.role" label="Role" />
+            <QBtn label="Submit" type="submit" color="secondary" />
+        </QForm>
 </template>
 
 <style scoped lang="scss">
